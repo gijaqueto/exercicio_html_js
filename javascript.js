60c280ff9346f0763effd08693448c74a923f844
Giovanna Jaqueto <gijaqueto1@gmail.com>
2023-06-29 21:57:56 -0300

    mudancas

--- javascript.js
+++ javascript.js
@@ -1,21 +1,23 @@
+const form = document.getElementById('validacao');
 let numA = document.getElementById('numA');
 numA = parseInt(numA)
 
 let numB = document.getElementById('numB');
 numB = parseInt(numB)
 
+    form.addEventListener('submit', function(e){
+e.preventDefault();
+
 const mensagemSucesso = `O segundo número é maior que o primeiro!`;
 const mensagemErro = `O primeiro número é maior que o primeiro, tente novamente!`;
 
-function comparar(numA, numB) {
+function comparar(numA, numB) 
 
-    if numA > numB
-    {
+    if (numA > numB) {
         const containerMensagem = document.querySelector('mensagem');
         containerMensagem.innerHTML = mensagemErro;
     } else {
         const containerMensagem = document.querySelector('mensagem');
         containerMensagem.innerHTML = mensagemSucesso;
     }
-
-}
+})
