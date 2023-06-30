8ff118f15f93a7a81ae7c2cdf1b7ebcbbb587f51
Giovanna Jaqueto <gijaqueto1@gmail.com>
2023-06-30 15:26:12 -0300

    alteracoes

--- javascript.js
+++ javascript.js
@@ -1,23 +1,29 @@
 const form = document.getElementById('validacao');
 let numA = document.getElementById('numA');
-numA = parseInt(numA)
-
 let numB = document.getElementById('numB');
-numB = parseInt(numB)
 
-    form.addEventListener('submit', function(e){
+form.addEventListener('submit', function(e){
 e.preventDefault();
 
 const mensagemSucesso = `O segundo número é maior que o primeiro!`;
-const mensagemErro = `O primeiro número é maior que o primeiro, tente novamente!`;
+const mensagemErro = `O primeiro número é maior ou igual ao segundo, tente novamente!`;
 
-function comparar(numA, numB) 
+function comparar(numA, numB) {
+    return numB > numA
+}
 
-    if (numA > numB) {
-        const containerMensagem = document.querySelector('mensagem');
-        containerMensagem.innerHTML = mensagemErro;
-    } else {
-        const containerMensagem = document.querySelector('mensagem');
-        containerMensagem.innerHTML = mensagemSucesso;
-    }
+if (!comparar(numA.valueAsNumber, numB.valueAsNumber)) {
+
+	const containerMensagem = document.querySelector('.mensagem');
+
+    containerMensagem.innerHTML = mensagemErro;
+
+} else {
+
+	const containerMensagem = document.querySelector('.mensagem');
+
+    containerMensagem.innerHTML = mensagemSucesso;
+
+} 
+
 })
